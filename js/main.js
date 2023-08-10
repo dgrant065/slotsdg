fcb3626c55da0d5ff35fd0c93442d94ec81f7706
Darius Grant <d.grant065@gmail.com>
2023-08-10 13:09:42 -0400

    Add event listeners

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 /*----- constants -----*/
-
+const SLOTS_LOOKUP = {
+ 
+}
   
   /*----- state variables -----*/
   let winner, score, 
   
   /*----- cached elements  -----*/
-
+  
   
   /*----- event listeners -----*/
+document.querySelector('#spinner').addEventListener('click', spin)
+document.querySelector('#reset').addEventListener('click', init)
 
   /*----- functions -----*/
 
